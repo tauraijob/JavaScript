35af5e341f4889d728747c22b59ac8b0e8afba8a
Taurai Job Munodawafa <taujob1111@gmail.com>
2019-02-28 12:51:20 +0200

    Javascript
    Using external js file

---
+++ maths.js
@@ -0,0 +1,10 @@
+var x = 5;
+var y = 6;
+
+var z = x + y;
+var k = y-x;
+var l = x*y;
+var h = x/y; 
+
+document.getElementById("demo").innerHTML =
+"The value of z is: " + z  + "<br/>" + "The value of k is:"+ k +"<br/>" +"Value of l is:" + l + "<br/>" + "Value of h is:" +h;
